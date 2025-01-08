a73e8f47e8133005ec51877f3636d7db4f7c0c14
Samuel Sobalvarro <dev.msobalvarro@gmail.com>
2025-01-08 14:57:35 -0600

    restructure footer pafe

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router'
 
 export const Footer = () => {
   const { propierties } = useStorePropierties()
-  
+
   return (
     <footer className='flex flex-col backdrop-blur bg-gray-800/50 gap-10 md:flex-row justify-between items-center text-white p-8 lg:p-12 lg:py-4 w-full text-sm md:text-normal'>
       <figure className='bg-white p-6 rounded-full w-32 h-32 flex items-center justify-center'>
@@ -16,56 +16,54 @@ export const Footer = () => {
           alt='logo' />
       </figure>
 
-      <div className='flex flex-col gap-4 sm:text-sm xs:text-sm'>
-        <div className='flex items-center gap-4'>
-          <p>F8 Technologies {new Date().getFullYear()}</p>
-          <p>Todos los derechos reservados</p>
-          <Link to='/contact' className='text-sky-200 hover:underline'>Contacto</Link>
-        </div>
+      <div className='flex items-center md:text-lg justify-center gap-4 flex-1'>
+        <p>F8 Technologies {new Date().getFullYear()}</p>
+        <p>Todos los derechos reservados</p>
+        <Link to='/contact' className='text-sky-200 hover:underline'>Contacto</Link>
+      </div>
 
-        <div className='flex justify-center md:justify-end space-x-4 text-3xl'>
-          <a
-            href={propierties?.find(e => e.key === 'facebook')?.value}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='flex items-center justify-center w-12 h-12 bg-blue-600 rounded-full shadow-lg hover:shadow-xl transform hover:scale-110 transition-all duration-300 text-white'
-          >
-            <FaFacebook />
-          </a>
-          <a
-            href={propierties?.find(e => e.key === 'twitter')?.value}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='flex items-center justify-center w-12 h-12 bg-blue-400 rounded-full shadow-lg hover:shadow-xl transform hover:scale-110 transition-all duration-300 text-white'
-          >
-            <FaTwitter />
-          </a>
-          <a
-            href={propierties?.find(e => e.key === 'instagram')?.value}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='flex items-center justify-center w-12 h-12 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-full shadow-lg hover:shadow-xl transform hover:scale-110 transition-all duration-300 text-white'
-          >
-            <FaInstagram />
-          </a>
-          <a
-            // href='https://linkedin.com'
-            href={propierties?.find(e => e.key === 'linkedin')?.value}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='flex items-center justify-center w-12 h-12 bg-blue-700 rounded-full shadow-lg hover:shadow-xl transform hover:scale-110 transition-all duration-300 text-white'
-          >
-            <FaLinkedin />
-          </a>
-          <a
-            href={`wa.me/${propierties?.find(e => e.key === 'whatsapp')?.value}`}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='flex items-center justify-center w-12 h-12 bg-green-500 rounded-full shadow-lg hover:shadow-xl transform hover:scale-110 transition-all duration-300 text-white'
-          >
-            <FaWhatsapp />
-          </a>
-        </div>
+      <div className='flex justify-center md:justify-end space-x-3 text-xl'>
+        <a
+          href={propierties?.find(e => e.key === 'facebook')?.value}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='flex items-center justify-center w-12 h-12 bg-blue-600 rounded-full shadow-lg hover:shadow-xl transform hover:scale-110 transition-all duration-300 text-white'
+        >
+          <FaFacebook />
+        </a>
+        <a
+          href={propierties?.find(e => e.key === 'twitter')?.value}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='flex items-center justify-center w-12 h-12 bg-blue-400 rounded-full shadow-lg hover:shadow-xl transform hover:scale-110 transition-all duration-300 text-white'
+        >
+          <FaTwitter />
+        </a>
+        <a
+          href={propierties?.find(e => e.key === 'instagram')?.value}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='flex items-center justify-center w-12 h-12 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 rounded-full shadow-lg hover:shadow-xl transform hover:scale-110 transition-all duration-300 text-white'
+        >
+          <FaInstagram />
+        </a>
+        <a
+          // href='https://linkedin.com'
+          href={propierties?.find(e => e.key === 'linkedin')?.value}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='flex items-center justify-center w-12 h-12 bg-blue-700 rounded-full shadow-lg hover:shadow-xl transform hover:scale-110 transition-all duration-300 text-white'
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href={`wa.me/${propierties?.find(e => e.key === 'whatsapp')?.value}`}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='flex items-center justify-center w-12 h-12 bg-green-500 rounded-full shadow-lg hover:shadow-xl transform hover:scale-110 transition-all duration-300 text-white'
+        >
+          <FaWhatsapp />
+        </a>
       </div>
     </footer>
   )
--- src/components/ui/title.tsx
+++ src/components/ui/title.tsx
@@ -7,15 +7,15 @@ interface Props {
 }
 
 export const UiTitle = ({ title, description }: Props) => (
-  <header className='flex flex-col md:flex-row justify-between rounded-xl items-center backdrop-blur items-top gap-4 p-10 bg-gray-800/[0.2] border-2 border-slate-900 w-full'>
+  <header className='flex flex-col md:flex-row justify-between rounded-xl items-center backdrop-blur items-top gap-4 p-10 bg-gray-800/[0.2] border-2 border-slate-600 w-full'>
     <LazyLoadImage
       src={logo}
-      className='w-64'
+      className='w-64 mr-4'
       alt='logo' />
 
-    <view className='flex flex-col gap-4 items-end'>
-      <h1 className='text-4xl self-center md:self:end md:text-right lg:text-6xl'>{title}</h1>
-      <p className='text-gray-500 self-center md:self:end lg:text-2xl break-normal lg:max-w-[80%]'>
+    <view className='flex flex-col gap-4 items-center md:items-end'>
+      <h1 className='text-4xl md:text-right lg:text-6xl'>{title}</h1>
+      <p className='text-gray-500 lg:text-2xl break-normal text-center md:text-right'>
         {description}
       </p>
     </view>
